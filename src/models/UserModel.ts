a97c22128a447fd6ea7f6efa22ea8217180987f9
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2023-07-12 22:33:19 +0300

    Update changes

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+import mongoose,{ Document, Schema, ConnectOptions } from 'mongoose';
+import { IItem } from './ItemModel';
 
-const { Schema } = mongoose;
 
 const mongoURI = 'mongodb://127.0.0.1:27017/founditpracticedb';
 
@@ -10,12 +10,22 @@ mongoose.connect(mongoURI)
     console.log('MongoDB connected');
     // Start your server or perform other operations
   })
-  .catch((error : any) => {
+  .catch((error) => {
     console.error('MongoDB connection error:', error);
     // Handle the connection error
   });
 
-const userSchema = new Schema({
+
+export interface IUser extends Document {
+  name: string;
+  password: string;
+  email: string;
+  phone: string;
+  items: IItem[];
+  img?: string | null;
+}
+
+const userSchema = new Schema<IUser>({
   name: {
     type: String,
     required: true,
@@ -41,6 +51,7 @@ const userSchema = new Schema({
   img: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<IUser>('User', userSchema);
 
-module.exports = User;
+export default User;
+ 
