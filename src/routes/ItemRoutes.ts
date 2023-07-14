9bfa1652977bb2ee44a50b0c27f373831b8da090
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2023-07-15 02:20:09 +0300

    Update changes

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import Item, { IItem } from '../models/ItemModel';
-
+import User from '../models/UserModel';
 const router = express.Router();
 
 // Route to get all items
@@ -9,8 +9,40 @@ router.get('/', (req, res) => {
   });
 
 // Route to create a new item
-router.post('/', (req, res) => {
-    // Handle POST request for items
+router.post('/insertItem', (req, res) => {
+  const { category, date, place, description, ownerId, images } = req.body;
+
+  // Ensure the types of the extracted parameters
+  const categoryValue: string = category;
+  const dateValue: Date = new Date(date);
+  const placeValue: string = place;
+  const descriptionValue: string = description;
+  const ownerIdValue: string = ownerId;
+  const imagesValue: (string | undefined)[] = images;
+
+  const newItem = new Item({
+    category: categoryValue,
+    date: dateValue,
+    place: placeValue,
+    description: descriptionValue,
+    ownerId: ownerIdValue,
+    images: imagesValue,
+  });
+  
+  // Save the new item to the database
+  newItem.save()
+    .then((savedItem) => {
+      // Populate the ownerId with the User document
+      return User.findByIdAndUpdate(ownerId, { $push: { items: savedItem._id } }, { new: true });
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Item inserted successfully' });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    });
+
   });
 
 export default router;
