f035e0ce73cb260922d37e005d74b9e002680e79
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2023-07-21 15:30:16 +0300

    Update changes

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -5,11 +5,9 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import config from '../config/firebase.config';
 import Item, { IItem } from '../models/ItemModel';
 import User from '../models/UserModel';
-import uploadImagesToFirebase from '../middlewares/UploadImagesToFirebase';
 
 initializeApp(config.firebaseConfig);
 const storage = getStorage();
-const upload = multer({ storage: multer.memoryStorage() });
 const router: Router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
@@ -24,50 +22,57 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // Route to create a new item
-router.post('/insertItem', upload.array('images', 10), async (req: Request, res: Response) => {
+router.post('/insertItem', async (req: Request, res: Response) => {
+  const itemObj = req.query; // Access the itemObj as query parameters
+
+  // Access other formData fields
+  // For example, if you expect multiple files, you can access them like this:
+  const files = req.files;
+
+  // Process the data as needed
+
+  console.log(itemObj);
+  console.log(files);
+
+  res.sendStatus(200);
+  
   // Images will be available in req.files as Express.Multer.File[]
-  const images = req.files as Express.Multer.File[];
-
-  // Check if 'images' is defined and an array
-  if (!Array.isArray(images) || images.length === 0) {
-    res.status(400).json({ error: 'Please provide images in the request' });
-    return;
-  }
-
-  // Get the rest of the post properties from the query string
-  const { place, category, description, ownerId, date } = req.query;
-
-  try {
-    // Process the images array here
-    const arrayOfPaths = await uploadImagesToFirebase(images);
-
-    const categoryValue: string = category as string;
-    const dateValue: Date = new Date(date as string);
-    const placeValue: string = place as string;
-    const descriptionValue: string = description as string;
-    const ownerIdValue: string = ownerId as string;
-    const imagesValue: string[] = arrayOfPaths;
-
-    const newItem = new Item({
-      category: categoryValue,
-      date: dateValue,
-      place: placeValue,
-      description: descriptionValue,
-      ownerId: ownerIdValue,
-      images: imagesValue,
-    });
-
-    // Save the new item to the database
-    const savedItem = await newItem.save();
-
-    // Populate the ownerId with the User document
-    await User.findByIdAndUpdate(ownerIdValue, { $push: { items: savedItem._id } }, { new: true });
-
-    res.status(200).json({ message: 'Post Uploaded Successfully !' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Server error' });
-  }
+// const images = req.files as Express.Multer.File[];
+//
+// // Check if 'images' is defined and an array
+//
+//
+// // Get the rest of the post properties from the query string
+// const { place, category, description, ownerId, date } = req.query;
+//
+// try {
+//   // Process the images array here
+//
+//   const categoryValue: string = category as string;
+//   const dateValue: Date = new Date(date as string);
+//   const placeValue: string = place as string;
+//   const descriptionValue: string = description as string;
+//   const ownerIdValue: string = ownerId as string;
+//
+//   const newItem = new Item({
+//     category: categoryValue,
+//     date: dateValue,
+//     place: placeValue,
+//     description: descriptionValue,
+//     ownerId: ownerIdValue,
+//   });
+//
+//   // Save the new item to the database
+//   const savedItem = await newItem.save();
+//
+//   // Populate the ownerId with the User document
+//   await User.findByIdAndUpdate(ownerIdValue, { $push: { items: savedItem._id } }, { new: true });
+//
+//   res.status(200).json({ message: 'Post Uploaded Successfully !' });
+// } catch (err) {
+//   console.error(err);
+//   res.status(500).json({ error: 'Server error' });
+// }
 });
 
 
