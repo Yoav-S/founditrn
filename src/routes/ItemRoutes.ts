03d8223aa46747c3994d1e5e7d1eec97a4fd3181
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2023-07-23 14:51:45 +0300

    Update changes

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -30,25 +30,39 @@ router.get('/', async (req: Request, res: Response) => {
 // Route to create a new item
 router.post('/insertItem', async (req: Request, res: Response) => {
   try {
-    const { place, category, description, ownerId } = req.body; // Extract other fields from req.body
-    const images = req.files as Express.Multer.File[]; // Extract the uploaded images as an array
+    const { place, category, description, ownerId } = req.body;
+    const images = req.files as Express.Multer.File[];
     console.log('place', place);
     console.log('category', category);
     console.log('description', description);
     console.log('ownerId', ownerId);
     console.log('images', images);
 
-    // Create a new item in the database
-  //  const newItem: IItem = await Item.create({
-  //    place,
-  //    category,
-  //    description,
-  //    ownerId,
-  //    images: images.map(image => ({ url: image.path })), // Save the paths of uploaded images to the database
-  //  });
-  //  console.log(newItem);
+    // Upload images to Firebase Storage and get download URLs
+    const imageUrls: string[] = [];
+    for (const image of images) {
+      const imageRef = ref(storage, `images/${image.filename}`);
+      await uploadBytes(imageRef, image.buffer);
+      const downloadUrl = await getDownloadURL(imageRef);
+      imageUrls.push(downloadUrl);
+    }
 
-    res.status(200);
+    // Generate current date and time
+    const currentDate = new Date();
+
+    // Create a new item in the database
+    const newItem: IItem = await Item.create({
+      place,
+      category,
+      description,
+      ownerId,
+      images: imageUrls.map(url => ({ url })), // Save the download URLs of uploaded images to the database
+      date: currentDate, // Save the current date and time to the database
+    });
+
+    console.log('New item created:', newItem);
+
+    res.status(200).json(newItem);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
