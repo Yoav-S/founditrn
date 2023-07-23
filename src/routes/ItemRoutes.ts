71e8d75fd18cce52589be687f1a774d546d2ec76
Yoav-S <99165953+Yoav-S@users.noreply.github.com>
2023-07-23 12:30:22 +0300

    Update changes

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -6,6 +6,7 @@ import User from '../models/UserModel';
 import firebaseApp from '../config/firebase.config'; // Import the Firebase app here
 const storage = getStorage(firebaseApp);
 const router: Router = Router();
+const upload = multer({ dest: 'uploads/' }); // Set the destination folder for uploaded files
 
 // Your existing routes and code
 
@@ -28,8 +29,13 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // Route to create a new item
-router.post('/insertItem', async (req: Request, res: Response) => {
-  console.log(req); 
+router.post('/insertItem', upload.array('images'), async (req: Request, res: Response) => {
+  // Access the uploaded files
+  const images = req.files; // The uploaded files are available in req.files array
+
+  // Your code to handle the uploaded images
+  // ...
+
   res.sendStatus(200);
 });
 
